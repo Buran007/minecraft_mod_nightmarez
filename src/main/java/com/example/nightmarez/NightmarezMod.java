4cbc52ce7858b4a3dfecdf7a66aaffafb32c8bf5
Buran007 <vips.974@mail.ru>
2025-09-13 14:24:41 +0500

    1

--- src/main/java/com/example/nightmarez/NightmarezMod.java
+++ src/main/java/com/example/nightmarez/NightmarezMod.java
@@ -1,24 +1,23 @@
 package com.example.nightmarez;
 
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
-// Аннотация @Mod теперь берется из пакета net.neoforged.fml.common.Mod
 @Mod(NightmarezMod.MODID)
 public class NightmarezMod {
     public static final String MODID = "nightmarez";
 
     public NightmarezMod(IEventBus modEventBus) {
-        // Используем NeoForge.EVENT_BUS для регистрации глобальных событий
+        // Регистрируем глобальные события
         NeoForge.EVENT_BUS.register(new ModEventHandler());
 
-        // Регистрируем метод setup на шине событий мода
+        // Регистрируем setup
         modEventBus.addListener(this::setup);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // Код инициализации мода
+        // Здесь больше ничего не нужно для BreakBlockGoal или ModEventHandler
     }
 }
